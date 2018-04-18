1c4dcb6512dfbf71bca5b74a3707cbeca9022e88
SaiBhatla <38411885+SaiBhatla@users.noreply.github.com>
2018-04-17 23:12:38 -0400

    Merging changes
    merging changes

--- src/controls/Inputs/XPHMaskedTextBox.jsx
+++ src/controls/Inputs/XPHMaskedTextBox.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import kendo from '@progress/kendo-ui';
 import { MaskedTextBox} from '@progress/kendo-inputs-react-wrapper';
-import { Validator } from '@progress/kendo-validator-react-wrapper';
 
 export class XPHMaskedTextBox extends React.Component {
 
@@ -12,16 +11,40 @@ export class XPHMaskedTextBox extends React.Component {
 
     onChange(event) {
         const updatedText = event.sender.raw();
+        let isValid = true;
         //const updatedText = event.sender.value();
+        if(this.props.regex === undefined || 
+            (this.props.regex !== undefined && this.validateRegex(updatedText, this.props.regex))){
+            isValid = true;
+        }
+        else{
+            isValid = false;
+            }
+
+        this.SetStyle(isValid);
+
         if(this.props.onChange !== undefined){
             this.props.onChange(updatedText);
         }
-        
     }
+
+    validateRegex(inputText, regex) {
+        if (String(inputText).match(regex)) {
+            return true;
+        } else {
+            return false;
+        }
+        return false;
+    }
+
+    SetStyle(isValid){
+        console.log(isValid);
+    }
+
 
     render() {
         return (
-            <div>
+            <div style={{ display: 'inline-block' }}>
                    <MaskedTextBox  
                         value={this.props.value}
                         mask={this.props.mask}
